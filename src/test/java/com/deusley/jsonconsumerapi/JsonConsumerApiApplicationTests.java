90f33037e783069112380ccbb90fd6f5dfe0d05b
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-06-02 15:11:57 -0300

    firstCommit

---
+++ src/test/java/com/deusley/jsonconsumerapi/JsonConsumerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusley.jsonconsumerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
